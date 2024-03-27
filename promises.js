94997bae5c5b3b195029b20bd6cb338d2cce2fd6
gtoro45 <113807617+gtoro45@users.noreply.github.com>
2024-03-26 19:48:34 -0500

    Add files via upload

---
+++ promises.js
@@ -0,0 +1,69 @@
+// returns a promise containing the address LatLng informtaion
+// since it is a callback function
+function geocodePromise(address, service) {
+  console.log(address);
+  return new Promise(function(resolve, reject) {
+    service.geocode({'address': address}, function(results, status) {
+      if(status == google.maps.GeocoderStatus.OK) {
+        //resolve
+        resolve({
+          latitude: results[0].geometry.location.lat(), 
+          longitude: results[0].geometry.location.lng()
+        });
+      }
+      else {
+        //reject
+        reject("Geolocation data not resolved for " + address);
+      }
+    });
+  })
+}
+
+// returns a promise containing a list of nearby locations based
+// on the source and distance radius
+function nearbyLocationsPromise(source, distance=0) {
+  return new Promise(function(resolve, reject) {
+    // call the places service to get an array of nearby places within a
+    // radius of size distance from the source, and use to find waypoints
+    let sourceLatLng = new google.maps.LatLng(source.latitude, source.longitude);
+    let placesService = new google.maps.places.PlacesService(map);
+    var request = {
+      location: sourceLatLng, 
+      radius: '4000'
+    };
+
+    placesService.nearbySearch(request, function(result, status) {
+      if(status == google.maps.places.PlacesServiceStatus.OK) {
+        // resolve
+        resolve(result);
+      //   console.log("The list: ");
+      //   console.log(result);
+      }
+      else {
+        // reject
+        reject("Could not get nearby locations for " + source);
+      }
+    });
+
+  });
+}
+
+// returns a promise containing the distance data between points pointA and pointB
+// in the traveling order A --> B, where pointA and pointB are Places Objects
+function distancePromise(pointA, pointB) {
+  //TODO: make the request from pointA and pointB
+  var request = {};
+  return new Promise(function(resolve, reject) {
+    var distanceMatrixService = new google.maps.DistanceMatrixService();
+    distanceMatrixService.getDistanceMatrix(request, function(result, status) {
+      if(status == google.maps.DistanceMatrixStatus.OK) {
+        //resolve
+        //TODO
+      }
+      else {
+        //reject
+        //TODO
+      }
+    });
+  });
+}
---
+++ rrtest.js
@@ -0,0 +1,97 @@
+var map;
+
+//this functions creates the actual map we are able to see in the HTML page 
+function initMap() {
+  // assigns directions services to javacript constants to avoid typing everything asll over again
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer({draggable:true,}); //draggable makes it so that we can drag the legs of the loop and modify
+
+  console.log("Live");
+
+  //create the map and assigns it to the div in HTML with the corresponding Id
+  map = new google.maps.Map(document.getElementById("map"), {
+    //30.6149996259997, -96.34153199058316 A&M University coordinates
+    center: { lat: 30.614, lng: -96.34},
+    zoom: 15,
+  });
+
+  directionsRenderer.setMap(map);
+
+  //Uses Places API so that it autocompletes when typing in the inputs, have to do it for each input
+  //each input is given a different Id in HTML
+  let sourceAutocomplete = new google.maps.places.Autocomplete(document.getElementById("source"));
+
+  //when the button with the "submit" id is click the calcRoute function runs
+  document.getElementById("submit").addEventListener("click", () => {
+    route(directionsService, directionsRenderer);
+  });
+}
+
+// main logic function calls a ton of promises
+function route(directionsService, directionsRenderer) {
+  //get the source address and distance information 
+  var sourceAddr = document.getElementById("source").value; 
+  var distance = document.getElementById("distance").value;
+  var geocoderService = new google.maps.Geocoder();
+  
+  // get source geolocation data -> returned by promise as sourceGeoLoc
+  geocodePromise(sourceAddr, geocoderService).then((sourceGeoLoc => {
+    // use the source data to get a list of nearby waypoints -> returned by promise as nearbyList
+    nearbyLocationsPromise(sourceGeoLoc).then((nearbyList) => {
+      // TODO: write route algorithm
+			let waypoints = generateCorners(distance, sourceGeoLoc, nearbyList);
+      console.log(nearbyList);
+
+			// call displayRoute() to show on map
+			displayRoute(waypoints, directionsService, directionsRenderer);
+    
+    // catch any promise rejections
+    }).catch((error) => {
+      console.log(error);
+    })
+  })).catch((error) => {
+    console.log(error);
+  });
+}
+
+// corner generating algorithm based on the source and its
+// nearby waypoints
+function generateCorners(distance, source, nearby) {
+  // TODO
+
+	// dummy return
+	return [{location: {lat: source.latitude, lng: source.longitude}}];
+}
+
+// calcRoute function is defined and the google services will need to be passed as parameters since we will use
+// them here again
+function displayRoute(waypnt, directionsService, directionsRenderer){
+  //gets the values typed by the user into the HTML inputs and assigns it to a javascript variable 
+  var source = document.getElementById("source").value; 
+
+  //object that will be used in directionsService.route 
+  let request = {
+      origin: source,
+      destination: source, //source is assigned to both origin and destination since we want a closed loop
+      waypoints: waypnt, //waypoint array used here 
+      travelMode: "WALKING", //this can be changed to DRIVING, BICYCLING, TRANSIT, etc
+      //there are more details that can be added to this object but not necessary for this program
+  };
+
+  //asks the directions api for a route using the previously defined object
+  //and a callback function
+  directionsService.route(request, function(result, status){
+      if(status == 'OK'){
+          //renders the route on the map
+          directionsRenderer.setDirections(result); 
+      }
+  });
+}
+
+
+//runs the initMap functions, which sequentially calls the others functions
+window.initMap = initMap;
+
+
+ 
+ 
